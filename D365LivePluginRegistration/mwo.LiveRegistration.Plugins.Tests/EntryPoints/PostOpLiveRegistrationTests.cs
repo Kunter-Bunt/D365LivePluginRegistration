6ac3c31663a9911e2d99d4decf34f6cb66a658f9
Marius Wodtke <marius.wodtke@gmail.com>
2020-07-07 14:09:57 +0200

    Added tests for integration of ImageManager

--- D365LivePluginRegistration/mwo.LiveRegistration.Plugins.Tests/EntryPoints/PostOpLiveRegistrationTests.cs
+++ D365LivePluginRegistration/mwo.LiveRegistration.Plugins.Tests/EntryPoints/PostOpLiveRegistrationTests.cs
@@ -71,6 +71,39 @@ namespace mwo.LiveRegistration.Plugins.Tests.EntryPoints
         }
 
         [TestMethod]
+        public void Create_ImageCreatedTest()
+        {
+            //Arrange
+            var target = new Entity("mwo_pluginstepregistration")
+            {
+                ["mwo_plugintypename"] = PluginTypeName,
+                ["mwo_sdkmessage"] = Create,
+                ["mwo_primaryentity"] = PrimaryEntityName,
+                ["mwo_secondaryentity"] = null,
+                ["mwo_stepconfiguration"] = null,
+                ["mwo_asynchronous"] = true,
+                ["mwwo_pluginstepstage"] = new OptionSetValue(122870040),
+                ["mwo_filteringattributes"] = null,
+                ["mwo_description"] = null,
+                ["mwo_managed"] = true,
+                ["mwo_imagetype"] = new OptionSetValue(122870002),
+                ["mwo_imagename"] = "Default",
+                ["mwo_imageattributes"] = null,
+            };
+
+            var ctx = CreateContext(target, null, "Create");
+
+            //Act
+            Context.ExecutePluginWith<PreOpLiveRegistration>(ctx);
+
+            //Assert
+            var steps = Context.CreateQuery("sdkmessageprocessingstepimage").ToList();
+            Assert.AreEqual(1, steps.Count);
+
+            target.Id = Service.Create(target); //This is here for the other tests that rely on the Entity being there.
+        }
+
+        [TestMethod]
         public void Update_ChangePersistsTest()
         {
             //Arrange
@@ -91,6 +124,29 @@ namespace mwo.LiveRegistration.Plugins.Tests.EntryPoints
             var steps = Context.CreateQuery("sdkmessageprocessingstep").ToList();
             Assert.AreEqual(1, steps.Count);
             Assert.AreEqual(desc, steps.First().GetAttributeValue<string>("description"));
+        }
+
+        [TestMethod]
+        public void Update_ImageChangeTest()
+        {
+            //Arrange
+            Create_ImageCreatedTest();
+            var preImage = Context.CreateQuery("mwo_pluginstepregistration").First();
+
+            var name = "newName";
+            var target = new Entity("mwo_pluginstepregistration", preImage.Id)
+            {
+                ["mwo_imagename"] = name,
+            };
+            var ctx = CreateContext(target, preImage, "Update");
+
+            //Act
+            Context.ExecutePluginWith<PreOpLiveRegistration>(ctx);
+
+            //Assert
+            var steps = Context.CreateQuery("sdkmessageprocessingstepimage").ToList();
+            Assert.AreEqual(1, steps.Count);
+            Assert.AreEqual(name, steps.First().GetAttributeValue<string>("entityalias"));
         }
 
         [TestMethod]
@@ -153,6 +209,24 @@ namespace mwo.LiveRegistration.Plugins.Tests.EntryPoints
 
             //Assert
             var steps = Context.CreateQuery("sdkmessageprocessingstep").ToList();
+            Assert.AreEqual(0, steps.Count);
+        }
+
+        [TestMethod]
+        public void Delete_NoMoreImageTest()
+        {
+            //Arrange
+            Create_ImageCreatedTest();
+            var preImage = Context.CreateQuery("mwo_pluginstepregistration").First();
+
+            var target = new Entity("mwo_pluginstepregistration", preImage.Id);
+            var ctx = CreateContext(target, preImage, "Delete");
+
+            //Act
+            Context.ExecutePluginWith<PreOpLiveRegistration>(ctx);
+
+            //Assert
+            var steps = Context.CreateQuery("sdkmessageprocessingstepimage").ToList();
             Assert.AreEqual(0, steps.Count);
         }
 
